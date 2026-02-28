19ee3ab5399c7599fbeced9bf39e967c20e25cd4
merel-de-klein <mereldeklein@live.nl>
2026-02-28 09:55:12 +0100

    Always return StashItemEnriched from api/stash

--- src/api/stash.ts
+++ src/api/stash.ts
@@ -1,6 +1,6 @@
-import { stashItems, statuses, tags } from "@/lib/stash-data";
+import { stashItems, tags } from "@/lib/stash-data";
 import { StashItem, StashItemEnriched, Tag } from "@/types/stash";
-import { getCategoriesByGroup, getGroupById } from "@/utils/stash-utils";
+import { getCategoriesByGroup, getCategoryById, getGroupById, getStatusById } from "@/utils/stash-utils";
 
 export const getTags = async (): Promise<Tag[]> => tags;
 
@@ -10,8 +10,35 @@ export const resolveTags = async (tagIds: number[]): Promise<Tag[]> => {
     .filter((tag): tag is Tag => tag !== undefined);
 };
 
-export const getCurrentStash = async (): Promise<StashItem[]> => {
-  return stashItems.filter((item) => item.statusId === 2);
+const enrichStashItem = async (item: StashItem): Promise<StashItemEnriched|undefined> => {
+  const category = getCategoryById(item.categoryId);
+  const group = getGroupById(category?.groupId || 0);
+  const status = getStatusById(item.statusId);
+
+  if (!category || !group || !status) return;
+
+  const tags = await resolveTags(item.tagIds);
+
+  return {
+    ...item,
+    group,
+    category,
+    status,
+    tags,
+  };
+};
+
+export const getStashItemBySlug = async (slug: string): Promise<StashItemEnriched|undefined> => {
+  const item = stashItems.find((i) => i.slug === slug);
+  return item ? enrichStashItem(item) : undefined;
+};
+
+export const getCurrentStash = async (): Promise<StashItemEnriched[]> => {
+  const items = stashItems.filter((item) => item.statusId === 2);
+  const itemPromises = items.map(enrichStashItem);
+  const results = await Promise.all(itemPromises);
+
+  return results.filter((item) => item !== undefined);
 }
 
 export const getStashByGroup = async (groupId: number): Promise<StashItemEnriched[]> => {
@@ -20,15 +47,10 @@ export const getStashByGroup = async (groupId: number): Promise<StashItemEnriche
 
   const filteredSortedItems = stashItems
     .filter(item => categoryIds.has(item.categoryId))
-    .sort((a, b) => a.id - b.id); // Ascending order
+    .sort((a, b) => a.id - b.id);
 
-  const itemPromises = filteredSortedItems.map<Promise<StashItemEnriched>>(async (item) => ({
-    ...item,
-    group: getGroupById(groupId)!,
-    category: categories.find(c => c.id === item.categoryId)!,
-    status: statuses.find(s => s.id === item.statusId)!,
-    tags: await resolveTags(item.tagIds)
-  }));
+  const itemPromises = filteredSortedItems.map(enrichStashItem);
+  const results = await Promise.all(itemPromises);
 
-  return Promise.all(itemPromises);
+  return results.filter((item) => item !== undefined);
 }
