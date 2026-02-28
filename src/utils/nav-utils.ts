49fb6df344ed0caf4a9e70258bc745e74a5829c0
merel-de-klein <mereldeklein@live.nl>
2026-02-28 09:54:51 +0100

    Add getStashItemUrl to nav-utils

--- src/utils/nav-utils.ts
+++ src/utils/nav-utils.ts
@@ -1,3 +1,5 @@
-import { Group } from "@/types/stash";
+import { Group, StashItemEnriched } from "@/types/stash";
 
 export const getGroupUrl = (group: Group) => `/stash/${group.slug}`;
+
+export const getStashItemUrl = (stash: StashItemEnriched) => getGroupUrl(stash.group) + `/${stash.slug}`;
